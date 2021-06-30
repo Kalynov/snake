b71f910f2eb76d912a6b77f0663978a4ff7d6ce4
akalinov <a.kalynov@netris.ru>
2021-06-30 15:32:28 +0300

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider } from 'react-redux';
-import store from './ARSs/reducers';
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
